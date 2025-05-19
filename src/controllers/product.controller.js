47886e6a40c214907d6b5e34107a8e188889376c
Marcio Montivero <montivero.marcio@gmail.com>
2025-05-19 18:23:30 -0300

    Se agregaron validaciones en api de productos

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,39 +1,84 @@
 import productService from "../services/product.service.js";
 import validateProduct from "../validator/product.validator.js";
+import validateObjectId from "../validator/objectID.validator.js";
 
 class ProductController {
     async get(req, res) {
-        const products = await productService.get();
-        res.status(200).json(products);
+        try {
+            const products = await productService.get();
+            if (!products) {
+                return res.status(404).json({ error: "Products not found" });
+            }
+            return res.status(200).json(products);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
     }
 
     async getOne(req, res) {
-        const { pid } = req.params;
-        const product = await productService.getOne(pid);
-        res.json(product);
+        try {
+            const { pid } = req.params;
+            if (!validateObjectId(pid)) {
+                return res.status(400).json({ error: "Invalid product id" });
+            }
+            const product = await productService.getOne(pid);
+            if (!product) {
+                return res.status(404).json({ error: "Product not found" });
+            }
+            return res.json(product);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
     }
 
     async create(req, res) {
-        const product = req.body;
-        const validProduct = await validateProduct.validateAll(product);
-        if (!validProduct.valid) {
-            return res.status(400).json(validProduct.reason);
+        try {
+            const product = req.body;
+            const validProduct = await validateProduct.validateCreate(product);
+            if (!validProduct.valid) {
+                return res.status(400).json(validProduct.reason);
+            }
+            const newProduct = await productService.create(product);
+            return res.status(201).json(newProduct);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
         }
-        const newProduct = await productService.create(product);
-        res.status(201).json(newProduct);
     }
 
     async update(req, res) {
-        const { pid } = req.params;
-        const product = req.body;
-        const updatedProduct = await productService.update(pid, product);
-        res.json(updatedProduct);
+        try {
+            const { pid } = req.params;
+            if (!validateObjectId(pid)) {
+                return res.status(400).json({ error: "Invalid product id" });
+            }
+            const product = req.body;
+
+            const validProduct = await validateProduct.validateUpdate(product);
+            if (!validProduct.valid) {
+                return res.status(400).json(validProduct.reason);
+            }
+
+            const updatedProduct = await productService.update(pid, product);
+            if (!updatedProduct) {
+                return res.status(404).json({ error: "Product not found" });
+            }
+            return res.json(updatedProduct);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
     }
 
     async delete(req, res) {
-        const { pid } = req.params;
-        const deletedProduct = await productService.delete(pid);
-        res.json(deletedProduct);
+        try {
+            const { pid } = req.params;
+            if (!validateObjectId(pid)) {
+                return res.status(400).json({ error: "Invalid product id" });
+            }
+            const deletedProduct = await productService.delete(pid);
+            return res.json(deletedProduct);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
     }
 }
 
--- src/validator/product.validator.js
+++ src/validator/product.validator.js
@@ -1,33 +1,37 @@
 import productService from "../services/product.service.js";
 
-const validateTypeFields = (product) => {
-    if (typeof product.title !== "string")
-        return { valid: false, reason: "Title must be a string" };
-    if (typeof product.description !== "string")
-        return { valid: false, reason: "Description must be a string" };
-    if (typeof product.code !== "string")
-        return { valid: false, reason: "Code must be a string" };
-    if (typeof product.price !== "number" || product.price < 0)
-        return {
-            valid: false,
-            reason: "Price must be a non-negative number",
-        };
-    if (typeof product.status !== "boolean")
-        return { valid: false, reason: "Status must be a boolean" };
-    if (typeof product.stock !== "number" || product.stock < 0)
-        return {
-            valid: false,
-            reason: "Stock must be a non-negative number",
-        };
-    if (typeof product.category !== "string")
-        return { valid: false, reason: "Category must be a string" };
-    if (product.thumbnails && !Array.isArray(product.thumbnails))
-        return { valid: false, reason: "Thumbnails must be an array" };
-    return { valid: true };
-};
-
 class ProductValidator {
-    validateFields(product) {
+    async existsCode(product) {
+        const existsProduct = await productService.getOneByCode(product.code);
+        return !existsProduct ? true : false;
+    }
+
+    validateCreateTypeFields(product) {
+        if (typeof product.title !== "string")
+            return { valid: false, reason: "Title must be a string" };
+        if (typeof product.description !== "string")
+            return { valid: false, reason: "Description must be a string" };
+        if (typeof product.code !== "string")
+            return { valid: false, reason: "Code must be a string" };
+        if (typeof product.price !== "number" || product.price < 0)
+            return {
+                valid: false,
+                reason: "Price must be a non-negative number",
+            };
+        if (typeof product.status !== "boolean")
+            return { valid: false, reason: "Status must be a boolean" };
+        if (typeof product.stock !== "number" || product.stock < 0)
+            return {
+                valid: false,
+                reason: "Stock must be a non-negative number",
+            };
+        if (typeof product.category !== "string")
+            return { valid: false, reason: "Category must be a string" };
+        if (product.thumbnails && !Array.isArray(product.thumbnails))
+            return { valid: false, reason: "Thumbnails must be an array" };
+        return { valid: true };
+    }
+    validateCreateFields(product) {
         const requiredFields = [
             "title",
             "description",
@@ -50,23 +54,72 @@ class ProductValidator {
                 };
         }
 
-        const typeValidation = validateTypeFields(product);
+        const typeValidation = this.validateCreateTypeFields(product);
         if (!typeValidation.valid) return typeValidation;
 
         return { valid: true };
     }
-
-    async existsCode(product) {
-        const existsProduct = await productService.getOneByCode(product.code);
-        return !existsProduct ? true : false;
-    }
-
-    async validateAll(product) {
-        const fieldValidation = this.validateFields(product);
+    async validateCreate(product) {
+        const fieldValidation = this.validateCreateFields(product);
         if (!fieldValidation.valid) return fieldValidation;
 
         const uniqueCode = await this.existsCode(product);
         if (!uniqueCode) return { valid: false, reason: "Code already exists" };
+        return { valid: true };
+    }
+
+    validateUpdateTypeFields(product) {
+        if (product.title !== undefined && typeof product.title !== "string")
+            return { valid: false, reason: "Title must be a string" };
+        if (
+            product.description !== undefined &&
+            typeof product.description !== "string"
+        )
+            return { valid: false, reason: "Description must be a string" };
+        if (product.code !== undefined && typeof product.code !== "string")
+            return { valid: false, reason: "Code must be a string" };
+        if (
+            product.price !== undefined &&
+            (typeof product.price !== "number" || product.price < 0)
+        )
+            return {
+                valid: false,
+                reason: "Price must be a non-negative number",
+            };
+        if (product.status !== undefined && typeof product.status !== "boolean")
+            return { valid: false, reason: "Status must be a boolean" };
+        if (
+            product.stock !== undefined &&
+            (typeof product.stock !== "number" || product.stock < 0)
+        )
+            return {
+                valid: false,
+                reason: "Stock must be a non-negative number",
+            };
+        if (
+            product.category !== undefined &&
+            typeof product.category !== "string"
+        )
+            return { valid: false, reason: "Category must be a string" };
+        if (
+            product.thumbnails !== undefined &&
+            !Array.isArray(product.thumbnails)
+        )
+            return { valid: false, reason: "Thumbnails must be an array" };
+        return { valid: true };
+    }
+
+    async validateUpdate(product) {
+        const typeValidation = this.validateUpdateTypeFields(product);
+
+        if (!typeValidation.valid) return typeValidation;
+
+        if (
+            product.code !== undefined &&
+            (await this.existsCode(product.code))
+        ) {
+            return { valid: false, reason: "Code already exists" };
+        }
         return { valid: true };
     }
 }
