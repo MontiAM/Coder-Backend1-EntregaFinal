25f68fe9a8d4a5383a0b5431a6f8354ff93155c5
Marcio Montivero <montivero.marcio@gmail.com>
2025-05-20 22:36:08 -0300

    Anotaciones de consignas en route

--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -3,11 +3,26 @@ import cartController from "../controllers/cart.controller.js";
 
 const router = Router();
 
-router.get("/", cartController.get);
+router.get("/", cartController.get); // Este ya estaba
 router.get("/:cid", cartController.getOne);
-router.post("/", cartController.create);
-router.delete("/:cid", cartController.delete);
-router.post("/:cid/product/:pid", cartController.addProductToCart);
+router.post("/", cartController.create); // Este ya estaba
+router.delete("/:cid", cartController.delete); // Modificar para que solo elimine los productos del carrito
+router.post("/:cid/product/:pid", cartController.addProductToCart); // Este ya estaba
 router.delete("/:cid/product/:pid", cartController.removeProductFromCart);
 
 export default router;
+
+// Agregar metodo PUT
+// api/carts/:cid
+// Debera actualizar todos los productos del carrito con un arreglo de productos
+
+// Agregar metodo PUT
+// api/carts/:cid/products/:pid
+// Debera poder actualizar SOLO la cantidad de ejemplares del producto
+// por cualquier cantidad pasada desde req.body
+
+// Agregar metodo DELETE
+// api/carts/:cid
+// Debera eliminar todos los productos del carrito
+
+// PARA TODOS AGREGAR EL POPULATE
