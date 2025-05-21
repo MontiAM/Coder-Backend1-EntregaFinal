a8e197cc25a726d04403c241800d1e03c37e145f
Marcio Montivero <montivero.marcio@gmail.com>
2025-05-20 23:00:44 -0300

    feat: configuracion de handlebars

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import mongoDB from "../db/connection.js";
+import configureHandlebars from "../configuration/handlebars.config.js";
+import __dirname from "../configuration/__dirname.js";
+import path from "path";
 import { error404Route, error500Route } from "./routes/error.route.js";
 import productRouter from "./routes/product.route.js";
 import cartRouter from "./routes/cart.route.js";
@@ -9,11 +12,18 @@ const app = express();
 // DB Connection
 await mongoDB.connect();
 
+// Handlebars
+configureHandlebars(app);
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./public")));
 
 // Routes
+app.get("/render", (req, res) => {
+    res.render("index");
+});
 app.use("/api/products", productRouter);
 app.use("/api/carts", cartRouter);
 app.use(error404Route);
